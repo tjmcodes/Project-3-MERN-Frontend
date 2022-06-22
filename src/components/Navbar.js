8c6c1a16f6362d68836d42431cc41f01d0ef0d39
kaz <kaz.e.mierz@gmail.com>
2022-06-22 16:29:16 +0100

    Navbar_uodate

---
+++ src/components/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const NavBar = () => {
+  return <nav className="navbar" role="navigation" aria-label="main navigation">
+    <div className="navbar-menu is-active">
+      <div className="navbar-end">
+        <div className="navbar-item">
+          <div className="buttons">
+            <Link to="/all-sounds" className="button is-dark">
+              <strong>Sounds</strong>
+            </Link>
+            <Link to="/register" className="button is-light">
+              Register
+            </Link>
+            <Link to="/login" className="button is-light">
+              Login
+            </Link>
+            <Link to='/sounds/new-sounds' className="button is-primary">
+              New Sounds
+            </Link>
+            <Link to='/sounds/sound-list' className="button is-primary">
+              All Sounds
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  </nav>
+}
+
+export default NavBar
