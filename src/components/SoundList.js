70781f0e2af6eba791414435dcbabb25b465d5d2
Teresa Morini <tjmcodes@gmail.com>
2022-06-30 17:19:03 +0100

    NavBar corrected

--- src/components/SoundList.js
+++ src/components/SoundList.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { Link } from 'react-router-dom'
 import SearchBar from './searchBar'
-import NavBar from './NavBar'
+import NavBar from './NavBar.js'
 
 
 const SoundList = () => {
